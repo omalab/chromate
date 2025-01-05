715efe63be105b08ffdfa8f111ce56418e23eea0
Eth3rnit3 <eth3rnit3@gmail.com>
2025-01-05 21:38:10 +0100

    core: Update BotBrowser installer

--- lib/bot_browser/downloader.rb
+++ lib/bot_browser/downloader.rb
@@ -5,13 +5,12 @@ require 'chromate/binary'
 module BotBrowser
   class Downloader
     class << self
-      def download(version = nil)
+      def download(version = nil, profile = nil, platform = :mac)
         version ||= versions.keys.first
+        profile ||= profiles[version].keys.first
         version       = version.to_sym
-        platform      = :mac
-        arch          = :arm64
-        binary_path   = download_file(versions[version][platform][arch][:binary], "/tmp/botbrowser_#{version}_#{platform}_#{arch}.dmg")
-        profile_path  = download_file(versions[version][platform][arch][:profile], "/tmp/botbrowser_#{version}_#{platform}_#{arch}.json")
+        binary_path   = download_file(versions[version][platform], "/tmp/botbrowser_#{version}_#{platform}.dmg")
+        profile_path  = download_file(profiles[version][profile], "/tmp/botbrowser_#{version}_#{platform}.json")
 
         [binary_path, profile_path]
       end
@@ -26,26 +25,25 @@ module BotBrowser
       def versions
         {
           v130: {
-            mac: {
-              arm64: {
-                binary: 'https://github.com/MiddleSchoolStudent/BotBrowser/releases/download/v130/botbrowser_130.0.6723.92_mac_arm64.dmg',
-                profile: 'https://raw.githubusercontent.com/MiddleSchoolStudent/BotBrowser/refs/heads/main/profiles/v130/chrome130-macarm.enc'
-              }
-            },
-            linux: {
-              x64: {
-                binary: 'https://github.com/MiddleSchoolStudent/BotBrowser/releases/download/v130/botbrowser_130.0.6723.117_amd64.deb',
-                # no specifi linux profile for moment
-                profile: 'https://raw.githubusercontent.com/MiddleSchoolStudent/BotBrowser/refs/heads/main/profiles/v130/chrome130-macarm.enc'
-              }
-            },
-            windows: {
-              x64: {
-                binary: 'https://github.com/MiddleSchoolStudent/BotBrowser/releases/download/v130/botbrowser_130.0.6723.117_win_x86_64.7z',
-                # no specific windows profile for moment
-                profile: 'https://raw.githubusercontent.com/MiddleSchoolStudent/BotBrowser/refs/heads/main/profiles/v130/chrome130-macarm.enc'
-              }
-            }
+            mac: 'https://github.com/MiddleSchoolStudent/BotBrowser/releases/download/v130/botbrowser_130.0.6723.92_mac_arm64.dmg',
+            linux: 'https://github.com/MiddleSchoolStudent/BotBrowser/releases/download/v130/botbrowser_130.0.6723.117_amd64.deb',
+            windows: 'https://github.com/MiddleSchoolStudent/BotBrowser/releases/download/v130/botbrowser_130.0.6723.117_win_x86_64.7z'
+          }
+        }
+      end
+
+      def profiles
+        {
+          v128: {
+            mac: 'https://raw.githubusercontent.com/MiddleSchoolStudent/BotBrowser/refs/heads/main/profiles/v128/chrome128_mac_arm64.enc',
+            win: 'https://raw.githubusercontent.com/MiddleSchoolStudent/BotBrowser/refs/heads/main/profiles/v128/chrome128_win10_x86_64.enc'
+          },
+          v129: {
+            mac: 'https://raw.githubusercontent.com/MiddleSchoolStudent/BotBrowser/refs/heads/main/profiles/v129/chrome129_mac_arm64.enc'
+          },
+          v130: {
+            mac: 'https://raw.githubusercontent.com/MiddleSchoolStudent/BotBrowser/refs/heads/main/profiles/v130/chrome130_mac_arm64.enc',
+            iphone: 'https://raw.githubusercontent.com/MiddleSchoolStudent/BotBrowser/refs/heads/main/profiles/v130/chrome130_iphone.enc'
           }
         }
       end
