9ee716da9725507a2a8c6d567d381cc224fc3684
William Flanagan <wflanagan@audienti.com>
2025-02-20 12:29:41 -0500

    Update stealth.rb
    Change the priority to US English.

--- lib/chromate/actions/stealth.rb
+++ lib/chromate/actions/stealth.rb
@@ -10,7 +10,7 @@ module Chromate
         # Define custom headers
         custom_headers = {
           'User-Agent' => UserAgent.call,
-          'Accept-Language' => 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,und;q=0.6,es;q=0.5,pt;q=0.4',
+          'Accept-Language' => 'en-US,us;q=0.9,fr-FR,q=0.8,en;q=0.7,und;q=0.6,es;q=0.5,pt;q=0.4',
           'Sec-CH-UA' => '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
           'Sec-CH-UA-Platform' => '"' + UserAgent.os + '"', # rubocop:disable Style/StringConcatenation
           'Sec-CH-UA-Mobile' => '?0'
